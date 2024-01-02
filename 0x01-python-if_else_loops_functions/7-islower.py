e0ee9544687d91df2c508669aa7c91142d0cb461
Abouzaid1 <am5715073@gmail.com>
2024-01-02 23:56:08 +0200

    upate

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == '' and c == ' ':
-        return 0
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        return 'a' <= c <= 'z'
+        return False
+    
