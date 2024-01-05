7cdc95167ebafa11b4c99e8562dd72cee60fad4a
Abouzaid1 <am5715073@gmail.com>
2024-01-05 02:21:44 +0200

    the third file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     result = 0
-    for i in range(count+1):
-        if i>0:
+    for i in range(count + 1):
+        if i > 0:
             result += int(sys.argv[i])
 print(result)
