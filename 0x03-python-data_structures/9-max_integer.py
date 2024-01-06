aad4d850e21577ef3671f289d665e011b2b1ddbe
Abouzaid1 <am5715073@gmail.com>
2024-01-06 18:51:15 +0200

    Fourth Folder

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    maxValue = 0
+    for i in range(0, len(my_list)):
+        if i < len(my_list) - 1:
+            if my_list[i] >= my_list[i + 1]:
+                if maxValue >= my_list[i]:
+                    continue
+                else: maxValue = my_list[i]
+            else:
+                maxValue = my_list[i+1]
+                continue
+    return maxValue
