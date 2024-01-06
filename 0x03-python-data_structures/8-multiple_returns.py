c0f681958ab63280c6d500515e0441125bd04873
Abouzaid1 <am5715073@gmail.com>
2024-01-06 18:26:17 +0200

    Fourth Folder

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    tuple = ()
+    if not sentence:
+        tuple = 0, None
+    else:
+        tuple = (len(sentence), sentence[0])
+    return tuple
