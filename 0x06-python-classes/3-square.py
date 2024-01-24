c000f48a57576940c748b2a7677e7ba7f97936c8
Abouzaid1 <am5715073@gmail.com>
2024-01-24 18:07:39 +0200

    Third file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,7 @@ class Square:
             TypeError: if size is not integer
             ValueError: if size is less than zero
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")  
