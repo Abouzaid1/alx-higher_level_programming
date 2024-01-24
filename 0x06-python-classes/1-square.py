0e5ea2dcee72411b3849ff7a4ff2a11dfce10aac
Abouzaid1 <am5715073@gmail.com>
2024-01-24 11:31:40 +0200

    second file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class called square and give it size"""
+
+
+class Square:
+    """Square Class
+    Arg:
+    size (int): size sqaure 
+    """
+    def __init__(self,size):
+        self.__size = size
