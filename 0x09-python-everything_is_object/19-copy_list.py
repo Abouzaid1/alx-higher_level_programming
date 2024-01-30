483cb04277fb89b9c5a31d6c5fafd24cf355267f
Abouzaid1 <am5715073@gmail.com>
2024-01-30 19:32:15 +0200

    add files

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""function that print a copy"""
-
-
 def copy_list(l):
-    """Initialize a function  that return a copy list"""
-    new_list = []
-    new_list += l
-    return (new_list)
+    return l[:]
