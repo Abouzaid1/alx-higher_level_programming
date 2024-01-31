513dd758f471c9a00c68cc4d7ce6a78bd97dd68d
Abouzaid1 <am5715073@gmail.com>
2024-01-31 12:24:14 +0200

    classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,7 +7,12 @@ class Rectangle:
 
 
     def __init__(self, width=0, height=0):
-        """Initialize the first instance of a Rectangle"""
+        """Initialize the first instance of a Rectangle
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
