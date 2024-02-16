158b6bed3e492565e71e8a58f2615cac66a19f5a
Abouzaid1 <am5715073@gmail.com>
2024-02-16 21:48:43 +0200

    edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,6 +72,6 @@ class Rectangle(Base):
 
     def display(self):
         """The stdout of the rectangle"""
-        print("\n" * self.y ,end="")
-        for i in range(self.height):
-            print(" " * self.x ,"#" * self.width, end="")
+        s = '\n' * self.__y \
+            (' ' * self.__x + '#' * self.__width + '\n') * self.__height
+        print(s, end='')
