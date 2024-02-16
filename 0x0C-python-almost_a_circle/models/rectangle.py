e4892d45cea935b59284714d10e6c479a9d5479c
Abouzaid1 <am5715073@gmail.com>
2024-02-16 22:02:49 +0200

    edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,5 +79,5 @@ class Rectangle(Base):
     def __str__(self):
         """srt"""
         return "[{}] ({}) {}/{} - {}/{}".\
-            format(type(self).__name__, self.id, self.__width,
-                   self.__height, self.__x, self.__y)
+            format(type(self).__name__, self.id, self.width,
+                   self.height, self.x, self.y)
