bcbe298144e89f3688fec77a088b979c9a76b780
Abouzaid1 <am5715073@gmail.com>
2024-02-16 20:08:02 +0200

    edit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Module for base class"""
+
+
 class Base():
     """Base model"""
+    
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Initialize a new Base."""
         if id is not None:
