c8b1c56181cdbd754df3eea1608feb9c636ffb94
Abouzaid1 <am5715073@gmail.com>
2024-02-16 20:42:35 +0200

    edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """the rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Construct"""
         super().__init__(id)
@@ -20,6 +21,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -29,6 +34,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -38,6 +47,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if value < 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -47,4 +58,6 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if value < 0:
+            raise ValueError("y must be > 0")
         self.__y = value
